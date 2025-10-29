7336734ed7fc38dcf25ad74506640c0ca158d002
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-10-29 15:00:11 +0530

    env file added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config()
 const express = require("express");
 const app = express();
 
@@ -6,8 +8,7 @@ app.get("/api/user", (request, response)=>{
     response.status(200).json({name: "Sudish", email: "sk@gmail.com", id : 1});
 });
 
-
 app.listen(4000, (error)=>{
     if(error) console.log("Error "+ error);
-    console.log("Server is running on port 4000");
+    console.log("Server is running on port " +process.env.PORT ||3000 );
 })
